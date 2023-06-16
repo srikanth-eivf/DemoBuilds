0546a8b23abd6d4b34ff92113364636bebb75e6e
Srikanth R <srikanthr@eivf.net>
2023-06-16 12:12:43 -0500

    initial

---
+++ CertDemoWinApp/CertDemoWinApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CertDemoWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lblVersion = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(258, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Cert Demo APP";
+            label1.Click += label1_Click;
+            // 
+            // lblVersion
+            // 
+            lblVersion.AutoSize = true;
+            lblVersion.Location = new Point(753, 421);
+            lblVersion.Name = "lblVersion";
+            lblVersion.Size = new Size(35, 20);
+            lblVersion.TabIndex = 1;
+            lblVersion.Text = "v1.0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblVersion);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "CertDemoApp";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblVersion;
+    }
+}
---
+++ CertDemoWinApp/CertDemoWinApp/Form1.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using System.Reflection;
+
+namespace CertDemoWinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //AssemblyName currentAssembly = AssemblyName.GetAssemblyName(path);
+            //Version assemblyVersion = currentAssembly.Version;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
